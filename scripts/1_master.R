91633bc7f74752d9e7c6a667e049e6ad664d67df
Laura-BN <lbuitrago41@gmail.com>
2025-02-08 17:19:58 -0500

    .

---
+++ scripts/1_master.R
@@ -0,0 +1,33 @@
+#-----------------------------------------------------------------------------//
+# x
+# Fecha: 
+#-----------------------------------------------------------------------------//
+
+rm(list = ls())
+
+#-----------------------------------------------------------------------------//
+# 1. Ruta de los archivos ----
+#-----------------------------------------------------------------------------//
+
+# Users
+
+path_user = "H:/My Drive/1. General/3. Académico/3. Uniandes/Machine Learning + BD/Repos_GitHub"
+
+
+path_main = "ProblemSet1_G10"
+path_gen = file.path(path_user, path_main)
+
+document_path = file.path(path_gen, "document") 
+scripts_path  = file.path(path_gen, "scripts") 
+stores_path   = file.path(path_gen, "stores")
+view_path     = file.path(path_gen, "view")
+
+#-----------------------------------------------------------------------------//
+# 2. Paquetes ----
+#-----------------------------------------------------------------------------//
+
+require(pacman)
+p_load(tidyverse, 
+       rvest,
+       dplyr,
+       stargazer) 
