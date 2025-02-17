7f8d6e127b9859c235d8d3d1ad55daa21112636e
CamiloRamirezAs <ca.ramirez10@uniandes.edu.co>
2025-02-17 08:59:13 -0500

    Update 2_import.R

--- scripts/2_import.R
+++ scripts/2_import.R
@@ -5,18 +5,18 @@
 #-----------------------------------------------------------------------------//
 
 # Loop para descargar y unir los datos
-total_pages <- 10 
+total_pages <- 10
+geih_list <- list()  # Lista para almacenar las tablas
 
 for (i in 1:total_pages) {
   url <- paste0("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_", i, ".html")
   page <- read_html(url)
   tables <- page %>% html_table(fill = TRUE)
-  assign(paste0("geih", i), as.data.frame(tables[[1]])) 
+  geih_list[[i]] <- as.data.frame(tables[[1]])  # Guardar la tabla en la lista
 }
 
-# Unir los 10 chunks en un solo data frame
-geih_2018 <- bind_rows(geih1, geih2, geih3, geih4, geih5, 
-                       geih6, geih7, geih8, geih9, geih10)
+# Unir todas las tablas en un solo data frame
+geih_2018 <- bind_rows(geih_list)
 
 # Guardar 
 saveRDS(geih_2018, file.path(stores_path, "geih_2018.rds"))
